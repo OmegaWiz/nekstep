eec1386a5a5a45575041463228417cf3908f4b6f
OmegaWiz <kraiwish123@gmail.com>
2023-01-25 08:32:10 +0700

    lay out template html

--- app.py
+++ app.py
@@ -6,6 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/engineering')
+def index():
+    return render_template('engineering.html')
+
+@app.route('/science')
+def index():
+    return render_template('science.html')
+
+@app.route('/medicine')
+def index():
+    return render_template('medicine.html')
+
+@app.route('/others')
+def index():
+    return render_template('others.html')
 
 if __name__ == '__main__':
     app.run()
